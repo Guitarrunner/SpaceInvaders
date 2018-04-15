2e1ee825b3386fcda702c434f0dfc0c8ee7846b5
guitarrunner <jdag98228@gmail.com>
2018-04-15 01:36:39 -0600

    Break until morning para hacer balas

--- src/Creador.java
+++ src/Creador.java
@@ -16,20 +16,20 @@ public class Creador {
 		//marcador1++;
 		nivel=vida.getnivel();
 		//System.out.println(contador);
-		System.out.println(nivel);
+		//System.out.println(nivel);
 		if (nivel%2!=0) {
 			contador=0;
-			System.out.println(contador+"000000");
+			//System.out.println(contador+"000000");
 		}
 		if (nivel%5!=0) {
 			contador1=0;
-			System.out.println(contador1+"000000");
+			//System.out.println(contador1+"000000");
 		}
 		if (nivel%2==0) {
 			if (contador==0) {
 				manejador.objetos.agregar(new Enemigo(100,200,ID.Enemigo,J1));
 				contador=contador+1;
-				System.out.println("sup");;
+				//System.out.println("sup");;
 			}
 		
 			}
--- src/KeyInput.java
+++ src/KeyInput.java
@@ -4,8 +4,15 @@ import java.awt.event.KeyEvent;
 
 public class KeyInput extends KeyAdapter {
 	private Manejador manejador;
+	private boolean[] keyDown = new boolean[4];
 	public KeyInput(Manejador manejador) {
 		this.manejador = manejador;
+		keyDown[0] =false;
+		keyDown[1] =false;
+		keyDown[2] =false;
+		keyDown[3] =false;
+		
+		
 	}
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
@@ -13,10 +20,14 @@ public class KeyInput extends KeyAdapter {
 		while(i<manejador.objetos.size()) {
 			Objetos tmp= manejador.objetos.get(i);
 			if(tmp.getId() == ID.Jugador) {
-				if (key == KeyEvent.VK_UP) tmp.setVely(-5);
-				if (key == KeyEvent.VK_DOWN) tmp.setVely(5);
-				if (key == KeyEvent.VK_RIGHT) tmp.setVelx(5);
-				if (key == KeyEvent.VK_LEFT) tmp.setVelx(-5);
+				if (key == KeyEvent.VK_UP) {keyDown[0]=true;tmp.setVely(-5);}
+				if (key == KeyEvent.VK_DOWN) {keyDown[1]=true;tmp.setVely(5);}
+				if (key == KeyEvent.VK_RIGHT) {keyDown[2]=true;tmp.setVelx(5);}
+				if (key == KeyEvent.VK_LEFT) {keyDown[3]=true;tmp.setVelx(-5);}
+				//if (key == KeyEvent.VK_UP) {tmp.setVely(-5);}
+				//if (key == KeyEvent.VK_DOWN) {tmp.setVely(5);}
+				//if (key == KeyEvent.VK_RIGHT) {tmp.setVelx(5);}
+				//if (key == KeyEvent.VK_LEFT) {tmp.setVelx(-5);}
 			}
 			i+=1;
 		}
@@ -30,12 +41,20 @@ public class KeyInput extends KeyAdapter {
 			while(i<manejador.objetos.size()) {
 				Objetos tmp= manejador.objetos.get(i);
 				if(tmp.getId() == ID.Jugador) {
-					if (key == KeyEvent.VK_UP) tmp.setVely(0);
-					if (key == KeyEvent.VK_DOWN) tmp.setVely(0);
-					if (key == KeyEvent.VK_RIGHT) tmp.setVelx(0);
-					if (key == KeyEvent.VK_LEFT) tmp.setVelx(0);
+					if (key == KeyEvent.VK_UP) keyDown[0]=false;//tmp.setVely(0);
+					if (key == KeyEvent.VK_DOWN) keyDown[1]=false;//tmp.setVely(0);
+					if (key == KeyEvent.VK_RIGHT) keyDown[2]=false;//tmp.setVelx(0);
+					if (key == KeyEvent.VK_LEFT) keyDown[3]=false;//tmp.setVelx(0);
+					//if (key == KeyEvent.VK_UP) {tmp.setVely(0);}
+					//if (key == KeyEvent.VK_DOWN) {tmp.setVely(0);}
+					//if (key == KeyEvent.VK_RIGHT) {tmp.setVelx(0);}
+					//if (key == KeyEvent.VK_LEFT) {tmp.setVelx(-0);}
+					if(!keyDown[0] && !keyDown[1])tmp.setVely(0);
+					if(!keyDown[2] && !keyDown[3])tmp.setVelx(0);
 				}
 				i+=1;
+				
+				
 			}
 	}
 }
